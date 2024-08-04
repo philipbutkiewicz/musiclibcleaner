25b99efa1d403564628dd8dd86e08328452c1cda
Philip Butkiewicz <philip@kenja.com>
2024-08-04 09:23:41 +0200

    I know this is a bad tool

--- musiclibcleaner.py
+++ musiclibcleaner.py
@@ -7,7 +7,7 @@ import re
 import warnings
 from tqdm import tqdm
 from eyed3.plugins.art import ArtFile
-from logging import info, warn, error, debug
+from logging import info, warning, error, debug
 from pathlib import Path
 
 '''HACK: I really don\'t care about possible nested sets. Regex is complicated enough without having to deal with writing compliant and clear expressions.'''
@@ -27,7 +27,9 @@ logging.basicConfig(
 
 ap = argparse.ArgumentParser()
 ap.add_argument('-p', '--path', type=str, required=True, help='Library path')
-ap.add_argument('-o', '--output-path', type=str, required=True, help='Output path')
+ap.add_argument('-o', '--output-path', type=str, required=True, help='Report output path')
+ap.add_argument('-r', '--refresh', action=argparse.BooleanOptionalAction, help='Force refresh')
+ap.add_argument('-sa', '--skip-album-covers', action=argparse.BooleanOptionalAction, help='Skip album cover scan')
 
 args = vars(ap.parse_args())
 
@@ -42,16 +44,21 @@ def create_paths(library_path, output_path):
 
 
 def scan_library(library_path):
+    global args
+
     supported_formats = [ 'mp3', 'aac', 'm4a', 'ogg', 'alac', 'ape', 'wav', 'MP3', 'AAC', 'M4A', 'OGG', 'ALAC', 'APE', 'WAV' ]
 
     media_files = []
 
-    cache_path = os.path.join(library_path, 'files.mlc')
-    if os.path.exists(cache_path):
+    cache_path = os.path.join(library_path, 'files.json')
+    if os.path.exists(cache_path) and not args.get('refresh', False):
         debug(f'Loading file list from cache in "{cache_path}"...')
         with open(cache_path, 'r', encoding='utf-8') as cf:
             media_files = cf.read().rstrip().split('\n')
     else:
+        if os.path.exists(cache_path):
+            os.remove(cache_path)
+
         info(f'Scanning for compatible media in "{library_path}", this will take a very long time...')
         for file_format in supported_formats:
             media_files.extend([ str(path) for path in Path(os.path.join(library_path)).rglob(f'*.{file_format}') ])
@@ -69,9 +76,9 @@ def scan_library(library_path):
 def determine_erroneous_track_num(media_file_name, media_info_item):
     debug(f'Checking "{media_file_name}" for erroneous track number tag...')
     
-    clean = media_info_item['tags']['TRACKNUMBER'] is not None and isinstance(media_info_item['tags']['TRACKNUMBER'], str) and re.match('^[[1-9][0-9]{0,2}|100]|/[[[1-9][0-9]{0,2}|100]]$', media_info_item['tags']['TRACKNUMBER'])
+    clean = 'TRACKNUMBER' in media_info_item['tags'] and isinstance(media_info_item['tags']['TRACKNUMBER'], str) and re.match('^[[1-9][0-9]{0,2}|100]|/[[[1-9][0-9]{0,2}|100]]$', media_info_item['tags']['TRACKNUMBER'])
     
-    debug(f'"{media_file_name}" track number is "{media_info_item["tags"]["TRACKNUMBER"]}" - {"PASSED" if clean else "FAILED"}')
+    debug(f'"{media_file_name}" - TRACKNUMBER - {"PASSED" if clean else "FAILED"}')
     
     return not clean
 
@@ -79,9 +86,9 @@ def determine_erroneous_track_num(media_file_name, media_info_item):
 def determine_erroneous_disc_num(media_file_name, media_info_item):
     debug(f'Checking "{media_file_name}" for erroneous disc number tag...')
     
-    clean = media_info_item['tags']['DISCNUMBER'] is None or (isinstance(media_info_item['tags']['DISCNUMBER'], str) and (re.match('^[[1-9][0-9]{0,2}|100]|/[[[1-9][0-9]{0,2}|100]]$', media_info_item['tags']['DISCNUMBER']) or media_info_item['tags']['DISCNUMBER'] == ''))
+    clean = 'DISCNUMBER' not in media_info_item['tags'] or (isinstance(media_info_item['tags']['DISCNUMBER'], str) and (re.match('^[[1-9][0-9]{0,2}|100]|/[[[1-9][0-9]{0,2}|100]]$', media_info_item['tags']['DISCNUMBER']) or media_info_item['tags']['DISCNUMBER'] == ''))
     
-    debug(f'"{media_file_name}" disc number is "{media_info_item["tags"]["DISCNUMBER"]}" - {"PASSED" if clean else "FAILED"}')
+    debug(f'"{media_file_name}" - DISCNUMBER - {"PASSED" if clean else "FAILED"}')
     
     return not clean
 
@@ -89,22 +96,26 @@ def determine_erroneous_disc_num(media_file_name, media_info_item):
 def determine_erroneous_date(media_file_name, media_info_item):
     debug(f'Checking "{media_file_name}" for date tag...')
     
-    clean = media_info_item['tags']['DATE'] is not None and isinstance(media_info_item['tags']['DATE'], str) and re.match('^[0-9][0-9][0-9][0-9]$', media_info_item['tags']['DATE'])
+    clean = 'DATE' in media_info_item['tags'] and isinstance(media_info_item['tags']['DATE'], str) and re.match('^[0-9][0-9][0-9][0-9]$', media_info_item['tags']['DATE'])
     
-    debug(f'"{media_file_name}" date is "{media_info_item["tags"]["DATE"]}" - {"PASSED" if clean else "FAILED"}')
+    debug(f'"{media_file_name}" - DATE - {"PASSED" if clean else "FAILED"}')
     
     return not clean
 
 
 def scan_media_info(library_path, media_files):
+    global args
     media_info = {}
 
-    cache_path = os.path.join(library_path, 'info.mlc')
-    if os.path.exists(cache_path):
+    cache_path = os.path.join(library_path, 'info.json')
+    if os.path.exists(cache_path) and not args.get('refresh', False):
         info(f'Loading media info from cache in "{cache_path}"...')
         with open(cache_path, 'r', encoding='utf-8') as cf:
             media_info = json.loads(cf.read())
     else:
+        if os.path.exists(cache_path):
+            os.remove(cache_path)
+
         info(f'Scanning for media info in "{library_path}", grab a coffee...')
         index = 0
         for media_file in tqdm(media_files):
@@ -113,12 +124,12 @@ def scan_media_info(library_path, media_files):
             try:
                 with taglib.File(media_file, save_on_exit=False) as file_info:
                     if not file_info:
-                        warn(f'Warning: Failed to scan "{media_file}"!')
+                        warning(f'Warning: Failed to scan "{media_file}"!')
                         media_info[media_file] = {
                             'tags': {},
-                            'has_cover': False,
                             'errored': True,
-                            'erroneous_tags': []
+                            'erroneous_tags': [],
+                            'has_cover': False
                         }
                     else:
                         media_info_item = {
@@ -131,50 +142,67 @@ def scan_media_info(library_path, media_files):
                                 'DISCNUMBER': file_info.tags['DISCNUMBER'][0] if 'DISCNUMBER' in file_info.tags else None,
                                 'DATE': file_info.tags['DATE'][0] if 'DATE' in file_info.tags else None
                             },
-                            'has_cover': False,
                             'errored': False,
-                            'erroneous_tags': []
+                            'erroneous_tags': [],
+                            'has_cover': False
                         }
 
                         debug(f'Found tags for "{media_file}": {json.dumps(media_info_item["tags"])}')
-
-                        debug(f'Checking front album cover for "{media_file}"...')
-                        try:
-                            art_file = ArtFile(media_file)
-                            if not art_file.image_data and not art_file.mime_type:
-                                debug(f'"{media_info} has no front cover data')
-                                media_info_item['has_cover'] = False
-                            else:
-                                debug(f'"{media_info} has cover data with MIME type {art_file.mime_type}')
-                                media_info_item['has_cover'] = True
-                        except:
-                            warn(f'Warning: Failed to check for the front album cover for "{media_file}"!')
-                            media_info_item['has_cover'] = False
-                        
-                        debug(f'Checking "{media_file}" for erroneous tags...')
-                        if determine_erroneous_track_num(media_file, media_info_item):
-                            media_info_item['erroneous_tags'].append('TRACKNUMBER')
-                        if determine_erroneous_disc_num(media_file, media_info_item):
-                            media_info_item['erroneous_tags'].append('DISCNUMBER')
-                        if determine_erroneous_date(media_file, media_info_item):
-                            media_info_item['erroneous_tags'].append('DATE')
-                        
-                        media_info[media_file] = media_info_item
+                    
+                    media_info[media_file] = media_info_item
             except:
-                warn(f'Warning: Failed to scan "{media_file}"!')
+                warning(f'Warning: Failed to scan "{media_file}"!')
                 media_info[media_file] = {
                     'tags': {},
                     'has_cover': False,
                     'errored': True,
-                    'erroneous_tags': []
+                    'erroneous_tags': [],
+                    'has_cover': False
                 }
             
         if len(media_info):
-            info(f'Finished scanning for media info in "{library_path}", saving results in "{cache_path}"...')
+            info(f'Finished scanning for media info in "{library_path}"!')
             with open(cache_path, 'w', encoding='utf-8') as cf:
                 cf.write(json.dumps(media_info))
     
     info(f'Found {len(media_info)} media info entries!')
+
+    return media_info
+
+
+def scan_album_covers(media_files, media_info):
+    global library_path
+
+    info('Checking for front album cover...')
+    for media_file in tqdm(media_files):
+        media_info_item = media_info[media_file]
+        try:
+            art_file = ArtFile(media_file)
+            if not art_file.image_data and not art_file.mime_type:
+                debug(f'"{media_info} has no front cover data')
+                media_info_item['has_cover'] = False
+            else:
+                debug(f'"{media_info} has cover data with MIME type {art_file.mime_type}')
+                media_info_item['has_cover'] = True
+        except:
+            warning(f'Warning: Failed to check for the front album cover for "{media_file}"!')
+            media_info_item['has_cover'] = False
+
+    return media_info
+
+
+def scan_erroneous_tags(media_files, media_info):
+    global library_path
+
+    info('Checking for erroneous tags...')
+    for media_file in tqdm(media_files):
+        media_info_item = media_info[media_file]
+        if determine_erroneous_track_num(media_file, media_info_item) and 'TRACKNUMBER' not in media_info_item['erroneous_tags']:
+            media_info_item['erroneous_tags'].append('TRACKNUMBER')
+        if determine_erroneous_disc_num(media_file, media_info_item) and 'DISCNUMBER' not in media_info_item['erroneous_tags']:
+            media_info_item['erroneous_tags'].append('DISCNUMBER')
+        if determine_erroneous_date(media_file, media_info_item) and 'DATE' not in media_info_item['erroneous_tags']:
+            media_info_item['erroneous_tags'].append('DATE')
 
     return media_info
 
@@ -225,10 +253,12 @@ output_path = args.get('output_path')
 
 create_paths(library_path, output_path)
 media_files = scan_library(library_path)
-media_info = scan_media_info(library_path, media_files)
+media_info = scan_erroneous_tags(media_files, scan_media_info(library_path, media_files))
+if not args.get('skip_album_covers', False):
+    media_info = scan_album_covers(media_files, media_info)
 
 if len(media_info):
     info(f'Saving a report to "{output_path}"...')
-    html = dict_list_to_html([ media_info[key] for key in media_info if len(media_info[key]['erroneous_tags']) or not media_info[key]['has_cover'] or media_info[key]['errored'] ])
+    html = dict_list_to_html([ media_info[key] for key in media_info if len(media_info[key]['erroneous_tags']) or (not media_info[key]['has_cover'] and not args.get('skip_album_covers')) or media_info[key]['errored'] ])
     with open(os.path.join(output_path, 'report.html'), 'w', encoding='utf-8') as hf:
         hf.write(html)
